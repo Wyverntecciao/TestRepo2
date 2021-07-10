c8f7882ca029c9d7965d661a30e5a34ba0ff057a
Chris Wiggins <76021097+Wyverntecciao@users.noreply.github.com>
2021-07-10 12:26:14 -0500

    Update PythonWebScraper.py

--- PythonWebScraper.py
+++ PythonWebScraper.py
@@ -1,11 +1,26 @@
-print ("Starting");
-from urllib.request import urlopen
+print ("Starting"); #starting message
+
+#url open library
+from urllib.request import urlopen 
+
+#import BeautifulSoup for html parsing
+# try: 
+#     from BeautifulSoup.request import BeautifulSoup
+# except ImportError:
+#     from bs4 import BeautifulSoup
+
+#need to install beautifulsoup or another package to parse HTML
+
+#setting how to open page
 url = "https://www.kxan.com"
 page = urlopen(url)
-
-print(page)
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 
-print(html)
+parsed_html = BeautifulSoup(html)
+print(parsed_html)
+
+#tempscrape = html.find("site-header__weather-forecast__text")
+
+#print(tempscrape)
