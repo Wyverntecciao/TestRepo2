46808e03f944c3ea092b1715080d7a23485a3528
Chris Wiggins <76021097+Wyverntecciao@users.noreply.github.com>
2021-07-20 23:40:27 -0500

    Update SoupWebScraper.py

---
+++ SoupWebScraper.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+url = "https://www.kxan.com"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+
+print(soup.gettext())
